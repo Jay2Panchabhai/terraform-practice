042aeb880dd1a7a407b29a07d40d5e52b5c9a157
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-12 12:25:23 +0530

    Day-1

--- Day-1/ec2.tf
+++ Day-1/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_block" {
     instance_type = "t3.micro"
     key_name = "demo.key"
     vpc_security_group_ids = ["sg-0f034c503a772275a"] 
-    user_data = file("/root/terraform-practice/Day1/user_data.sh")
+    user_data = file("/root/terraform-practice/Day-1/user_data.sh")
 
   root_block_device {
     volume_size = "10"
