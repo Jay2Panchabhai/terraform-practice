c9e87161c6228ebf79b7677fa91e83e002db72d9
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-17 22:47:31 +0530

    asg

--- Day-5/output.tf
+++ Day-5/output.tf
@@ -1,10 +1,6 @@
-output "public_ip" {
-    value = module.ec2.public_ip
-}
-
-output "public_subnet_1_id" {
-  value = module.vpc.public_subnet_1_id
-}
+#output "public_subnet_1_id" {
+# value = module.vpc.public_subnet_1_id
+#}
 
 output "public_subnet_2_id" {
   value = module.vpc.public_subnet_2_id
@@ -12,4 +8,11 @@ output "public_subnet_2_id" {
 
 output "sg_id" {
     value = module.vpc.sg_id
+}
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "asg_name" {
+  value = module.asg.asg_name
 }
