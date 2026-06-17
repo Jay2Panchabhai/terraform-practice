2fb113d6d7578ce72801f1da2f6822588784d47b
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-17 13:59:03 +0530

    day-5

---
+++ Day-5/module/Ec-2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "public_instance" {
+    ami = var.ami 
+    instance_type = var.instance_type
+    key_name = var.key_name
+    subnet_id =  var.subnet_id 
+    vpc_security_group_ids = [var.sg_id]
+    user_data = file("/root/terraform-practice/Day-5/module/ec2/user_data.sh")
+    tags = {
+        Name = "public-instance"
+    }
+}
