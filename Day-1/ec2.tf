6dc2c4d4ead8a87b10d5c186cb70ed75216e2002
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-18 12:51:55 +0530

    update

--- Day-1/ec2.tf
+++ Day-1/ec2.tf
@@ -1,18 +1,10 @@
-resource "aws_instance" "ec2_block" {
-    ami = "ami-0e38835daf6b8a2b9"
-    instance_type = "t3.micro"
-    key_name = "demo.key"
-    vpc_security_group_ids = ["sg-02d54887c651c5f1b"] 
-    user_data = file("/root/terraform-practice/Day-1/user_data.sh")
+resource "aws_instance" "web" {
+  ami           = "ami-0e38835daf6b8a2b9" # Ubuntu 22.04 (Mumbai)
+  instance_type = "t2.micro"
 
-  root_block_device {
-    volume_size = "10"
-    volume_type = "gp3"
-    }
-
-    tags = {
-        Name = "tf-instance"
-    }
+  tags = {
+    Name = "Terraform-EC2"
+  }
 }
 
 
