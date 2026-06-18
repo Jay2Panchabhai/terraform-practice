e94ff36eb1c9fe0d94cae17c897646080b6bde71
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-18 12:49:32 +0530

    update

--- Day-1/ec2.tf
+++ Day-1/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_block" {
-    ami = "ami-0db56f446d44f2f09"
+    ami = "ami-0e38835daf6b8a2b9"
     instance_type = "t3.micro"
     key_name = "demo.key"
     vpc_security_group_ids = ["sg-02d54887c651c5f1b"] 
