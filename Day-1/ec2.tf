00cffb4704b651fc526ab1cd083f31b0987505eb
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-18 13:01:50 +0530

    update

--- Day-1/ec2.tf
+++ Day-1/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0e38835daf6b8a2b9" # Ubuntu 22.04 (Mumbai)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform-EC2"
