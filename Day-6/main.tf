22721feebaa9e410dc486e78b9000429dc8600be
Jay2Panchabhai <jay2004panchabhai@gmail.com>
2026-06-19 10:19:00 +0530

    loops

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -1,1 +1,18 @@
-resource "aws_s3_bucket" "s3" {}
+resource "aws_s3_bucket" "s3" {
+}
+
+#####TERRAFORM LOOPS#######
+resource "aws_instance" "ec_2" {
+    for_each = tomap({
+        ec2-1 = "t3.micro"
+        ec2-2 = "t2.small"
+        ec2-3 = "c7i-flex.large"
+    })
+
+    ami = "ami-0e38835daf6b8a2b9"
+    instance_type = each.value
+    tags = {
+         Name = each.key
+     }
+  
+}
